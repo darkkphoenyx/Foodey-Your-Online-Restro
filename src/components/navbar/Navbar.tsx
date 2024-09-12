ec1530f944486878851cb69aeaea27cf106f1782
Deepesh Sunuwar <deepgeneral33@gmail.com>
2024-09-12 23:26:30 +0545

    responsive design 1

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,81 +1,150 @@
 import { Link } from "react-router-dom";
+import CloseIcon from "@mui/icons-material/Close";
+import MenuIcon from "@mui/icons-material/Menu";
 import QueryBuilderIcon from "@mui/icons-material/QueryBuilder";
 import LocalMallIcon from "@mui/icons-material/LocalMall";
+import { useState } from "react";
 
 export const Navbar = () => {
+  // Use state to control sidebar visibility
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <>
       <section className="bg-white/85 h-[16vh] sticky top-0 z-50">
         <div className="banner border-b border-gray-400">
-          <div className=" container mx-auto banner flex items-center justify-between py-1 px-10">
-            <div className="left-section ">
+          <div className="container mx-auto banner flex items-center justify-center md:justify-between py-1 px-10">
+            <div className="left-section flex gap-1">
+              <QueryBuilderIcon fontSize="small" />
               <p className="flex items-center gap-1">
-                <QueryBuilderIcon fontSize="small"></QueryBuilderIcon> Mon-Fri:
-                8am - 11pm, Sat-Sun: 8am - 12pm
+                Mon-Fri: 8am - 11pm, Sat-Sun: 8am - 12pm
               </p>
             </div>
-            <div className="right-section"></div>
-            <ul className="flex justify-between gap-2 font-medium ">
-              <li className="hover:text-[--primary-text-color]">
-                <Link to={"/"}>About</Link>
-              </li>
-              <li className="border-x border-black px-2 hover:text-[--primary-text-color]">
-                <Link to={"/"}>Contact Us</Link>
-              </li>
-              <li className="hover:text-[--primary-text-color]">
-                <Link to={"/"}>Login/Register</Link>
-              </li>
-            </ul>
+            <div className="right-section hidden md:block">
+              <ul className="flex justify-between gap-2 font-medium">
+                <li className="hover:text-[--primary-text-color]">
+                  <Link to="/">About</Link>
+                </li>
+                <li className="border-x border-black px-2 hover:text-[--primary-text-color]">
+                  <Link to="/">Contact Us</Link>
+                </li>
+                <li className="hover:text-[--primary-text-color]">
+                  <Link to="/">Login/Register</Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
+
         <div className="nav container mx-auto py-3 flex justify-between items-center px-10">
           <div className="logo">
             <Link
-              to={"/"}
+              to="/"
               className="font-extrabold text-4xl text-[--primary-text-color] italic"
             >
               Foodey
             </Link>
           </div>
-          <div className="nav-elements">
+
+          {/* Nav Links for large screens */}
+          <div className="nav-elements hidden md:block">
             <ul className="flex gap-20 font-medium justify-between text-lg">
               <li className="hover:text-[--primary-text-color]">
-                <Link to={"/"}>Home</Link>
+                <Link to="/">Home</Link>
               </li>
               <li className="hover:text-[--primary-text-color]">
-                <Link to={"/"}>Menu</Link>
+                <Link to="/">Menu</Link>
               </li>
               <li className="hover:text-[--primary-text-color]">
-                <Link to={"/"}>Order Foods</Link>
+                <Link to="/">Order Foods</Link>
               </li>
               <li className="hover:text-[--primary-text-color]">
-                <Link to={"/"}>Blogs</Link>
+                <Link to="/">Blogs</Link>
               </li>
             </ul>
           </div>
-          <div className="contact flex gap-8">
-            <div className="call flex items-center">
-              <img
-                className="h-8 pr-1"
-                src="/assets/phone.svg"
-                alt="call icon"
-              />
-              <div className="flex flex-col items-center justify-center">
-                <p className="text-sm">Call us for Order</p>
-                <p className="font-bold text-[--accent-color] text-xl">
-                  01-4590670
-                </p>
+
+          {/* Contact and buttons */}
+          <div className="hidden md:block">
+            <div className="contact flex gap-8">
+              <div className="call flex items-center">
+                <img
+                  className="h-8 pr-1"
+                  src="/assets/phone.svg"
+                  alt="call icon"
+                />
+                <div className="flex flex-col items-center justify-center">
+                  <p className="text-sm">Call us for Order</p>
+                  <p className="font-bold text-[--accent-color] text-xl">
+                    01-4590670
+                  </p>
+                </div>
               </div>
+              <button className="border hover:border-[--primary-color] border-black px-3 rounded-full hover:text-[--primary-color] transition-all active:translate-y-0.5">
+                <span>
+                  <LocalMallIcon fontSize="small" />
+                </span>
+              </button>
+              <button className="text-[--third-text-color] bg-[--primary-color] px-4 py-2 rounded-3xl text-lg font-semibold hover:text-[--primary-text-color] hover:bg-white border border-[--primary-color] transition-all active:translate-y-0.5">
+                Order Online
+              </button>
+
+              {/* Sidebar toggle button for mobile */}
             </div>
-            <button className="border hover:border-[--primary-color] border-black px-3 rounded-full hover:text-[--primary-color] transition-all active:translate-y-0.5">
-              <span className="">
-                <LocalMallIcon fontSize="small"></LocalMallIcon>
-              </span>
-            </button>
-            <button className="text-[--third-text-color] bg-[--primary-color] px-4 py-2  rounded-3xl text-lg font-semibold hover:text-[--primary-text-color] hover:bg-white border border-[--primary-color] transition-all active:translate-y-0.5">
-              Order Online
-            </button>
           </div>
+          <div className="block md:hidden" onClick={toggleSidebar}>
+            <MenuIcon fontSize="large"></MenuIcon>
+          </div>
+
+          {/* Sidebar for mobile */}
+          {isSidebarOpen && (
+            <div className="md:hidden block absolute bg-yellow-500/80 top-0 text-black right-0 h-[100vh] w-52 z-50">
+              <ul className="flex flex-col gap-4 p-4 text-xl font-medium">
+                <div onClick={toggleSidebar}>
+                  <CloseIcon fontSize="large"></CloseIcon>
+                </div>
+                <li>
+                  <Link to="/" onClick={toggleSidebar}>
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/" onClick={toggleSidebar}>
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/" onClick={toggleSidebar}>
+                    Menu
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/" onClick={toggleSidebar}>
+                    Order Foods
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/" onClick={toggleSidebar}>
+                    Blogs
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/" onClick={toggleSidebar}>
+                    Contact Us
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/" onClick={toggleSidebar}>
+                    Login/Register
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </section>
     </>

e332b5fadcb8d7d7881c85ea77454e6a96fccc6a
Deepesh Sunuwar <deepgeneral33@gmail.com>
2024-09-12 14:27:10 +0545

    Footer design changes

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,32 +2,40 @@
 import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
 import CallOutlinedIcon from "@mui/icons-material/CallOutlined";
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import { Link } from "react-router-dom";
 export const Footer = () => {
   const listStyle = "cursor-pointer hover:text-[#ff0000]";
   return (
     <>
-      <div className="pt-40 bg-white bg-[url(/assets/brush2.webp)] bg-no-repeat bg-cover bg-center">
-        <div className="flex gap-8 items-center mb-10">
-          <h1 className="text-[40px] font-extrabold text-[#ff0000] border-r pr-16 mr-8 cursor-pointer">
-            FOODIE
-          </h1>
-          <div className="">
-            <h3 className="text-[28px] font-bold ">Subscription News</h3>
-            <p className="text-[14px]">Subscribe to the weekly newslatter.</p>
+      <div className="pt-40 bg-white bg-[url(/assets/brush4.webp)] bg-no-repeat bg-cover bg-center">
+        <div className="upper-section container mx-auto px-10 flex flex-wrap items-center justify-between mb-10">
+          <div className="logo">
+            <Link
+              to={"/"}
+              className="font-extrabold text-4xl text-[--primary-text-color] italic"
+            >
+              Foodey
+            </Link>
           </div>
-          <input
-            className="px-6 py-3 outline-none w-[500px] rounded-full text-[16px]"
-            type="email"
-            placeholder="Enter Email Address"
-          />
-          <button className="bg-black text-white rounded-full px-8 py-3">
-            Subscribe
-          </button>
+          <div className="subsciption details">
+            <h3 className="text-3xl font-bold ">Subscription News</h3>
+            <p className="">Subscribe to the weekly newslatter.</p>
+          </div>
+          <div className="email-section flex gap-4 flex-wrap">
+            <input
+              className="px-6 py-3 outline-none w-[500px] rounded-full text-[16px]"
+              type="email"
+              placeholder="Enter Email Address"
+            />
+            <button className="text-[--third-text-color] bg-black px-4 py-2  rounded-3xl text-lg font-semibold hover:text-black hover:bg-transparent border border-black transition-all active:translate-y-0.5">
+              Subscribe
+            </button>
+          </div>
         </div>
         <hr />
-        <div className="grid grid-cols-4 gap-10 mt-10">
-          <div>
-            <p className="mb-3 text-[#3c3c3c]">
+        <div className="flex justify-center gap-16 flex-wrap mt-12 container mx-auto px-10 pb-12">
+          <div className=" w-[250px]">
+            <p className="mb-4 text-[#3c3c3c]">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit.
               Temporibus corrupti libero velit?
             </p>
@@ -38,7 +46,7 @@ export const Footer = () => {
             <h3 className="text-[18px] font-bold">Opening Hours</h3>
             <p className="text-[#ff0000] font-bold">08:00 AM - 12:00 PM</p>
           </div>
-          <div>
+          <div className=" w-[250px]">
             <h3 className="mb-3 font-bold text-[18px]">Quick Links</h3>
             <ul className="flex flex-col gap-1 text-[#3c3c3c]">
               <li className={listStyle}>About Us</li>
@@ -48,7 +56,7 @@ export const Footer = () => {
               <li className={listStyle}>Contact Us</li>
             </ul>
           </div>
-          <div>
+          <div className=" w-[250px]">
             <h3 className="mb-3 font-bold text-[18px]">Our Menu</h3>
             <ul className="flex flex-col gap-1 text-[#3c3c3c]">
               <li className={listStyle}>Burgers</li>
@@ -58,7 +66,7 @@ export const Footer = () => {
               <li className={listStyle}>Cold Drinks</li>
             </ul>
           </div>
-          <div>
+          <div className=" w-[250px]">
             <h3 className="mb-3 font-bold text-[18px]">Get In Touch</h3>
             <ul className="flex flex-col gap-3 text-[#3c3c3c]">
               <li className="cursor-pointer gap-3 flex hover:text-[#ff0000]">
